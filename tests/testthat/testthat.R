eff063b6bd992bc6d5f1cafc0ac17a4696224414
Roland Krause <roland.krause@uni.lu>
2017-01-18 17:07:37 +0100

    very basic test

---
+++ tests/testthat/testthat.R
@@ -0,0 +1,12 @@
+context("oh well")
+library(testthat)
+library(ffars)
+
+test_that("true is true", {
+expect_equal(TRUE, TRUE)
+  expect_equal(TRUE, TRUE)
+  expect_equal(TRUE, TRUE)
+  expect_equal(FALSE, FALSE)
+
+})
+
