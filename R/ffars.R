c5b10153ab6f7048e45ebf02a68d42ba291d5013
Roland Krause <roland.krause@uni.lu>
2017-01-18 17:10:59 +0100

    Added Travis

--- R/ffars.R
+++ R/ffars.R
@@ -19,6 +19,80 @@ fars_read <- function(filename) {
   dplyr::tbl_df(data)
 }
 
+#' Read years
+#'
+#' Read FARS data into memory and select month and years
+# @inheritParams make_filename # actually does not work as advertised in the book
+#' @importFrom dplyr mutate select
+#' @return A \code{tbl_df} with month and years as columns
+#' @examples fars_read_years(c(2013, 2014))
+#' @note Uses \code{\link{make_filename}} internally, warns on invalid year
+#' @export
+fars_read_years <- function(years) {
+  lapply(years, function(year) {
+    file <- make_filename(year)
+    tryCatch({
+      dat <- fars_read(file)
+      dplyr::mutate(dat, year = year) %>%
+        dplyr::select(MONTH, year)
+    }, error = function(e) {
+      warning("invalid year: ", year)
+      return(NULL)
+    })
+  })
+}
+
+#' Summarize years by month
+#' @param years vector of years
+#' @return tbl_df with MONTH and the respective years as columns
+#' @importFrom dplyr bind_rows group_by summarize %>%
+#' @importFrom tidyr spread
+#' @examples fars_summarize_years(c(2013, 2014))
+
+fars_summarize_years <- function(years) {
+  dat_list <- fars_read_years(years)
+  dplyr::bind_rows(dat_list) %>%
+    dplyr::group_by(year, MONTH) %>%
+    dplyr::summarize(n = n()) %>%
+    tidyr::spread(year, n)
+}
+
+#' Map accidents by state and year
+#'
+#' Plots the location of accidents on maps of US states
+#'
+#' @param state.num The numerical ID of a US state
+#' @param year the year
+#' @importFrom dplyr filter
+#' @importFrom maps map
+#' @return A map of the state and location of accident - an image
+#'
+#' @examples fars_map_state(48, 2013)
+#' @note Requires dplyr and maps package. To be called at package level via 'importFrom'
+#' statements
+#' @note Errors on state numbers not included in the data package
+fars_map_state <- function(state.num, year) {
+  filename <- make_filename(year)
+  data <- fars_read(filename)
+  state.num <- as.integer(state.num)
+
+  if(!(state.num %in% unique(data$STATE)))
+    stop("invalid STATE number: ", state.num)
+  data.sub <- dplyr::filter(data, STATE == state.num)
+  if(nrow(data.sub) == 0L) {
+    message("no accidents to plot")
+    return(invisible(NULL))
+  }
+  is.na(data.sub$LONGITUD) <- data.sub$LONGITUD > 900
+  is.na(data.sub$LATITUDE) <- data.sub$LATITUDE > 90
+  with(data.sub, {
+    maps::map("state", ylim = range(LATITUDE, na.rm = TRUE),
+              xlim = range(LONGITUD, na.rm = TRUE))
+    graphics::points(LONGITUD, LATITUDE, pch = 46)
+  })
+}
+
+
 
 
 #' Create canonical filenmae
