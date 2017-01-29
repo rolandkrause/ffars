6821efc9e0e6449628515ffc807416cf32057b34
Roland Krause <roland.krause@uni.lu>
2017-01-29 14:36:26 +0100

    RC1

--- tests/testthat/testthat.R
+++ tests/testthat/testthat.R
@@ -2,12 +2,7 @@ context("FARS data test")
 library(testthat)
 library(ffars)
 
-test_that("true is true", {
-expect_equal(TRUE, TRUE)
-  expect_equal(TRUE, TRUE)
-  expect_equal(TRUE, TRUE)
-  expect_equal(FALSE, FALSE)
-})
+## A bit difficult to write the test when test data cannot be checked in
 
 test_that("make filename",{
   expect_equal(make_filename(2013), "accident_2013.csv.bz2")
